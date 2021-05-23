becb76294b9960b211a63a0f7fb6c7b07141b8ef
yusronab <>
2021-05-24 06:51:53 +0700

    first commit

---
+++ src/main/java/nomer1/mainStack.java
@@ -0,0 +1,20 @@
+package nomer1;
+public class mainStack {
+    public static void main(String[] args) {
+        stack newStack = new stack(10);
+        newStack.push(50);
+        newStack.printData();
+        newStack.push(100);
+        newStack.printData();
+        newStack.pop();
+        newStack.printData();
+        newStack.push(60);
+        newStack.printData();
+        newStack.push(80);
+        newStack.printData();
+        newStack.pop();
+        newStack.printData();
+        long bacaTop = newStack.peek();
+        System.out.println("nilai top = "+bacaTop);
+    }
+}
